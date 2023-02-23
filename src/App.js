eb7aa56ba376907f5bb0759825994a318bff1853
Iran <iranmendoza01@outlook.com>
2023-02-22 23:24:40 -0600

    Filtro slider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Slider from'./components/Home/Slider';
 import Menu from './components/Home/Navbar';
+import Cetac from './components/Home/SeccionCetac';
+import Descripcion from './components/Home/DescripcionSlider';
 
 function App() {
   return (
     <>
      <Menu></Menu>
      <Slider></Slider>
+     <Cetac></Cetac>
+     <Descripcion></Descripcion>
      
     </>
    
