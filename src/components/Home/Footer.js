0805c4bd1f0877bdad0757ab7adaf3219dfc8c2d
Iran <iranmendoza01@outlook.com>
2023-03-06 18:51:41 -0600

    Avance footer

--- src/components/Home/Footer.js
+++ src/components/Home/Footer.js
@@ -4,48 +4,57 @@ import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 import './Footer.css';
 import logo from './Imagenes/LogoFooter.png'
+import { ArrowRight, Facebook, Instagram, Phone, TelephoneFill, Whatsapp } from 'react-bootstrap-icons';
 
 function Footer() {
   return (
-    <>
-      <Row className='section-footer'>
+    <Row className='section-footer'>
 
-        <Col>
-            <Image src={logo} className='logo-footer'>
+      <Col>
+        <Container className='logo-section'>
+          <Image src={logo} className='logo-footer' />
+        </Container>
+        <Container className='name-footer'>
+          <p className='name-footer'>Centro de Estudios Tecnológicos en Aguas Continentales No. 05</p>
+        </Container>
 
-            </Image>
-        </Col>
+        <Container className='group-icons'>
+          <Facebook className='icons'/>
+          <Instagram className='icons'/>
+          <Whatsapp className='icons'/>
+          <TelephoneFill className='icons'/>
+        </Container>
+      </Col>
 
-        <Col>
-          <div>
-            <p className='sub'>ACERCA DE</p>
-            <p> </p>
-          </div>
-        </Col>
+      <Col>
+        <div>
+          <p className='sub'>ACERCA DE</p>
+          <p> </p>
+        </div>
+      </Col>
 
-        <Col>
-          <div>
-            <p className='sub'>OFERTA ACADÉMICA</p>
-            <p> </p>
-          </div>
-        </Col>
+      <Col>
+        <div>
+          <p className='sub'>OFERTA ACADÉMICA</p>
+          <p> </p>
+        </div>
+      </Col>
 
-                
-        <Col>
-          <div>
-            <p className='sub'>ASPIRANTES</p>
-            <p> </p>
-          </div>
-        </Col>
 
-        <Col>
-          <div>
-            <p className='sub'>ALUMNOS</p>
-            <p> </p>
-          </div>
-        </Col>
-      </Row>
-    </>
+      <Col>
+        <div>
+          <p className='sub'>ASPIRANTES</p>
+          <p> </p>
+        </div>
+      </Col>
+
+      <Col>
+        <div>
+          <p className='sub'>ALUMNOS</p>
+          <p> </p>
+        </div>
+      </Col>
+    </Row>
 
   );
 }
