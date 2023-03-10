9fd094bed86e49adbf284c55cf2bed24b0ad2745
Iran <iranmendoza01@outlook.com>
2023-03-10 15:09:40 -0600

    Carrusel Dinamico

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Cetac from './components/Home/SeccionCetac';
 import OfertaEducativa from './components/Home/OfertaEducativa';
 import Ubicacion from './components/Home/Ubicacion';
 import Noticias from './components/Home/Noticias';
+import CarruselNoticias from './components/Home/CarruselNoticias';
 import Footer from './components/Home/Footer';
 
 
@@ -17,6 +18,7 @@ function App() {
      <Cetac></Cetac>
      <OfertaEducativa></OfertaEducativa>
      <Noticias></Noticias>
+     <CarruselNoticias/>
      <Ubicacion></Ubicacion>
      <Footer></Footer>
     </>
---
+++ src/components/Home/CarruselNoticias.js
@@ -0,0 +1,89 @@
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+
+function CarruselNoticias() {
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
+    };
+    return (
+        <>
+            <Container>
+                <Row className='justify-content-center'>
+                    <p className='sub-section'>NOTICIAS</p>
+                </Row>
+            </Container>
+            <Carousel responsive={responsive} className="seccion-noticias">
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+
+            </Carousel>
+        </>
+    );
+}
+
+export default CarruselNoticias;
